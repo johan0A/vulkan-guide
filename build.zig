5566bac74264fcee56af8826c5fd04e59e9a05a4
johan0A <arnaudchesse@outlook.com>
2025-03-11 12:31:09 +0100

    init

---
+++ build.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const name = b.option([]const u8, "name", "set the name of the emitted binary");
+
+    const root_module = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    const vulkan = b.dependency("vulkan", .{
+        .registry = b.path("vendor/vk.xml"),
+    });
+    root_module.addImport("vulkan", vulkan.module("vulkan-zig"));
+
+    const glfw = b.dependency("glfw", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    root_module.linkLibrary(glfw.artifact("glfw"));
+
+    const c_translate = b.addTranslateC(.{
+        .root_source_file = b.addWriteFiles().add("c.c", "#include <GLFW/glfw3.h>"),
+        .target = target,
+        .optimize = optimize,
+    });
+    c_translate.addIncludePath(glfw.path("include"));
+    root_module.addImport("c", c_translate.createModule());
+
+    {
+        const exe = b.addExecutable(.{ .name = name orelse "zig-exe-template", .root_module = root_module });
+
+        b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| run_cmd.addArgs(args);
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+    }
+
+    {
+        const tests = b.addTest(.{ .name = name orelse "test", .root_module = root_module });
+
+        const run_tests = b.addRunArtifact(tests);
+        const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_tests.step);
+
+        const debug_tests_artifact = b.addInstallArtifact(tests, .{});
+        const debug_tests_step = b.step("build-test", "Create a test artifact that runs the tests");
+        debug_tests_step.dependOn(&debug_tests_artifact.step);
+    }
+
+    {
+        const exe_check = b.addExecutable(.{ .name = "check", .root_module = root_module });
+        const tests_check = b.addTest(.{ .name = "check", .root_module = root_module });
+
+        const check = b.step("check", "Check if exe and tests compile");
+        check.dependOn(&exe_check.step);
+        check.dependOn(&tests_check.step);
+    }
+}
---
+++ src/main.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+const vulkan = @import("vulkan");
+const c = @import("c");
+
+pub fn main() !void {}
